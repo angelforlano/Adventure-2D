46296b924698709a750e2901b2d8e422766f180b
Angel <angel12345leo@gmail.com>
2021-04-15 10:32:51 +0200

    *

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,8 +7,12 @@ public class Player : MonoBehaviour
     public int hp = 100;
     public float speed = 4;
     public float jumpForce = 4;
+    
+    public Animator animator;
     public SpriteRenderer renderer;
     public Rigidbody2D rigidbody;
+
+    private int jumps;
     
     // Start is called before the first frame update
     void Start()
@@ -25,10 +29,39 @@ public class Player : MonoBehaviour
 
         if (horizontal != 0)
             renderer.flipX = horizontal > 0;
+        
+        animator.SetBool("isMoving", horizontal != 0);
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumps < 2)
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Attack();
+        }
+
     }
 
     void Jump()
     {
+        jumps++;
         rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        animator.SetTrigger("jump");
+    }
+
+    void Attack()
+    {
+        animator.SetTrigger("attack");
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            jumps=0;
+            animator.SetTrigger("idle");
+        }
     }
 }
